81c9c3c59bce42c42532412843fda8f5488f4d8f
Harry Moss <h.moss@ucl.ac.uk>
2025-01-22 18:08:40 +0000

    Add a timer decorator

--- openalex_incremental_updater/core/utils.py
+++ openalex_incremental_updater/core/utils.py
@@ -2,27 +2,43 @@
 
 import functools
 import time
-from typing import Callable
+from typing import Callable, ParamSpec, TypeVar
 
 from loguru import logger
 
+P = ParamSpec("P")
+R = TypeVar("R")
 
-def simple_timer(timed_function: Callable) -> Callable:
+
+def simple_timer(
+    timed_function: Callable[P, R],
+) -> Callable[P, R]:
     """
-    Decorate a function to be timed.
+    Decorate a function with timer information and log this.
 
     Args:
-        timed_function (callable):  The function to time.
+        timed_function (Callable[P, R]): The function to time.
+
+    Returns:
+        Callable[P, R]: The wrapped function.
 
     """
 
     @functools.wraps(timed_function)
-    def wrapper_simple_timer(*args: list, **kwargs: dict) -> None:
+    def wrapper_simple_timer(*args: P.args, **kwargs: P.kwargs) -> R:
+        """
+        Wrap the called function with timer information.
+
+        Returns:
+            return_type: The result of the called function.
+
+        """
         start_time = time.perf_counter()
-        timed_function(*args, **kwargs)
+        result = timed_function(*args, **kwargs)
         end_time = time.perf_counter()
         logger.info(
             f"{timed_function.__name__}: Elapsed time: {(end_time - start_time):.2f} seconds"
         )
+        return result
 
     return wrapper_simple_timer
