f35617afc2df972d4471053cc3916957e4edd0c2
Harry Moss <h.moss@ucl.ac.uk>
2025-02-11 09:34:16 +0000

    Add comment to explain why id looks like (but isn't) an optional field in the database model. Adds psycopg2 dependency.

--- openalex_incremental_updater/models/data_store.py
+++ openalex_incremental_updater/models/data_store.py
@@ -12,7 +12,9 @@ from sqlmodel import Field, SQLModel
 class Work(SQLModel, table=True):
     """Define our unit of data based on OpenAlex Work."""
 
-    id: str = Field(primary_key=True, index=True)
+    # See https://sqlmodel.tiangolo.com/tutorial/automatic-id-none-refresh/ for a
+    # discussion on why we declare the type of `id` as optional, even though it's a primary key.
+    id: str | None = Field(primary_key=True, index=True, nullable=False)
     title: str
     authors: dict = Field(default_factory=dict, sa_column=Column(JSON))
     abstract: str | None
