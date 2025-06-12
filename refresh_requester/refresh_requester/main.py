83c45b3a50bb4a62c1eebcc22baddcc8e46f53dc
Harry Moss <h.moss@ucl.ac.uk>
2025-06-12 10:57:12 +0100

    Hotfix for undefined fetch date variable causing pydantic config errors on get_settings

--- refresh_requester/refresh_requester/main.py
+++ refresh_requester/refresh_requester/main.py
@@ -3,17 +3,23 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+from dotenv import load_dotenv
 from loguru import logger
 
 from refresh_requester.blob_storage import blob_upload, check_previous_file_dates
 from refresh_requester.config import get_settings
 from refresh_requester.jobs import run_job
 
+load_dotenv(override=True)
+
 
 def main() -> None:
     """Run the refresh requester job."""
-    if not get_settings().fetch_date:
-        fetch_date = check_previous_file_dates()
+    fetch_date = (
+        check_previous_file_dates()
+        if not get_settings().fetch_date
+        else get_settings().fetch_date
+    )
     logger.info(f"RUNNING JOB - Fetching data for date: {fetch_date}")
     data = run_job(fetch_date, limit=get_settings().limit)
 
