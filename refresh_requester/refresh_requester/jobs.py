068037511fd4b849451264f006e605042c9981ea
Harry Moss <h.moss@ucl.ac.uk>
2025-08-06 11:05:30 +0100

    Restructure full pipeline function out of main and into jobs - update tests - also start server in the if __name__=="__main__" call

--- refresh_requester/refresh_requester/jobs.py
+++ refresh_requester/refresh_requester/jobs.py
@@ -1,13 +1,20 @@
 """Define the job to request a refresh from OpenAlex."""
 
 import json
-from datetime import date
+import sys
+from datetime import date, datetime
+from zoneinfo import ZoneInfo
 
 from loguru import logger
 
-from refresh_requester.blob_storage import blob_upload
+from refresh_requester.blob_storage import BlobUploadError, blob_upload
 from refresh_requester.config import Settings
-from refresh_requester.openalex_refresh import request_refresh
+from refresh_requester.openalex_refresh import OpenAlexRefreshError, request_refresh
+from refresh_requester.repository import (
+    ImportSourceType,
+    upload_blob_storage_contents_to_repository,
+)
+from refresh_requester.utils import get_fetch_date
 
 
 def run_refresh_job(settings: Settings, fetch_date: date, limit: int | None) -> str:
@@ -72,3 +79,58 @@ def run_ingestion_metadata_blob_upload_job(
     uploaded_blob = blob_upload(json.dumps(metadata), blob_name)
     logger.info(f"Uploaded destiny repository ingestion metadata: {uploaded_blob}")
     return uploaded_blob
+
+
+def run_full_pipeline(settings: Settings) -> None:
+    """
+    Run the full refresh requester job pipeline.
+
+    Args:
+        settings (Settings): The settings to use for the job.
+
+    """
+    fetch_date = get_fetch_date(settings)
+
+    date_today = datetime.now(ZoneInfo("UTC")).date()
+    data_source = ImportSourceType.OPEN_ALEX.value
+
+    logger.info(f"RUNNING JOB - Fetching data for date: {fetch_date}")
+    try:
+        data = run_refresh_job(settings, fetch_date, limit=settings.limit)
+    except OpenAlexRefreshError as refresh_error:
+        error_message = f"Error when requesting refresh: {refresh_error}"
+        logger.error(error_message)
+        sys.exit(1)
+    logger.info("Data fetched successfully, preparing to upload to blob storage.")
+    try:
+        uploaded_blob = run_openalex_refresh_blob_upload_job(
+            data, fetch_date, date_today
+        )
+    except BlobUploadError as upload_error:
+        logger.error(f"Error uploading data to blob storage: {upload_error}")
+        sys.exit(1)
+    logger.info(f"Data fetched for date: {fetch_date}, uploaded {date_today}")
+    logger.info("Ingesting blob storage contents to repository")
+    ingestion_metadata = upload_blob_storage_contents_to_repository(
+        settings, blob_to_upload=uploaded_blob
+    )
+    logger.info(
+        f"Data ingestion for {fetch_date} started for {len(ingestion_metadata.get("import_batch_ids"))} blobs."
+    )
+
+    metadata_output = {
+        "blob_name": uploaded_blob,
+        "import_record": ingestion_metadata.get("import_record").model_dump(mode="json")
+        if ingestion_metadata.get("import_record")
+        else None,
+        "import_batch_id": str(next(iter(ingestion_metadata["import_batch_ids"])))
+        if ingestion_metadata.get("import_batch_ids")
+        else None,
+    }
+    uploaded_path = run_ingestion_metadata_blob_upload_job(
+        metadata_output, data_source, fetch_date
+    )
+
+    logger.success(
+        f"Metadata for ingestion uploaded to blob storage: {settings.STORAGE_BLOB_CONTAINER}/{uploaded_path}"
+    )
--- refresh_requester/refresh_requester/main.py
+++ refresh_requester/refresh_requester/main.py
@@ -1,27 +1,15 @@
 """Main module for the refresh requester job."""
 
 import os
-import sys
+import socket
 import threading
-from datetime import datetime
-from zoneinfo import ZoneInfo
+import uuid
 
 from fastapi import FastAPI
 from loguru import logger
 
-from refresh_requester.blob_storage import BlobUploadError
-from refresh_requester.config import Settings, get_settings
-from refresh_requester.jobs import (
-    run_ingestion_metadata_blob_upload_job,
-    run_openalex_refresh_blob_upload_job,
-    run_refresh_job,
-)
-from refresh_requester.openalex_refresh import OpenAlexRefreshError
-from refresh_requester.repository import (
-    ImportSourceType,
-    upload_blob_storage_contents_to_repository,
-)
-from refresh_requester.utils import get_fetch_date
+from refresh_requester.config import get_settings
+from refresh_requester.jobs import run_full_pipeline
 
 health_probe_app = FastAPI()
 
@@ -51,61 +39,16 @@ def start_health_check_server() -> None:
     )
 
 
-def main(settings: Settings) -> None:
-    """Run the refresh requester job."""
+if __name__ == "__main__":
     if not os.environ.get("PYTEST_CURRENT_TEST"):
         health_check_thread = threading.Thread(
             target=start_health_check_server,
             daemon=True,  # Ensure the thread exits when the main program exits
         )
         health_check_thread.start()
-
-    fetch_date = get_fetch_date(settings)
-
-    date_today = datetime.now(ZoneInfo("UTC")).date()
-    data_source = ImportSourceType.OPEN_ALEX.value
-
-    logger.info(f"RUNNING JOB - Fetching data for date: {fetch_date}")
-    try:
-        data = run_refresh_job(settings, fetch_date, limit=settings.limit)
-    except OpenAlexRefreshError as refresh_error:
-        error_message = f"Error when requesting refresh: {refresh_error}"
-        logger.error(error_message)
-        sys.exit(1)
-    logger.info("Data fetched successfully, preparing to upload to blob storage.")
-    try:
-        uploaded_blob = run_openalex_refresh_blob_upload_job(
-            data, fetch_date, date_today
-        )
-    except BlobUploadError as upload_error:
-        logger.error(f"Error uploading data to blob storage: {upload_error}")
-        sys.exit(1)
-    logger.info(f"Data fetched for date: {fetch_date}, uploaded {date_today}")
-    logger.info("Ingesting blob storage contents to repository")
-    ingestion_metadata = upload_blob_storage_contents_to_repository(
-        settings, blob_to_upload=uploaded_blob
-    )
-    logger.info(
-        f"Data ingestion for {fetch_date} started for {len(ingestion_metadata.get("import_batch_ids"))} blobs."
+    logger.warning(
+        f"[DEBUG] Job started. Host: {socket.gethostname()}, Run ID: {uuid.uuid4()}"
     )
 
-    metadata_output = {
-        "blob_name": uploaded_blob,
-        "import_record": ingestion_metadata.get("import_record").model_dump(mode="json")
-        if ingestion_metadata.get("import_record")
-        else None,
-        "import_batch_id": str(next(iter(ingestion_metadata["import_batch_ids"])))
-        if ingestion_metadata.get("import_batch_ids")
-        else None,
-    }
-    uploaded_path = run_ingestion_metadata_blob_upload_job(
-        metadata_output, data_source, fetch_date
-    )
-
-    logger.success(
-        f"Metadata for ingestion uploaded to blob storage: {settings.STORAGE_BLOB_CONTAINER}/{uploaded_path}"
-    )
-
-
-if __name__ == "__main__":
-    main(get_settings())
+    settings = get_settings()
+    run_full_pipeline(settings)
